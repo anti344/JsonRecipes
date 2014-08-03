a0e2583ce20b7dbb24e3beffcea1467a391ab3e3
anti344 <an.bulah@ya.ru>
2014-08-03 19:33:20 +0300

    Fix headers.

--- src/main/scala/net/anti344/jsonrecipes/plugins/RecipeHandler.scala
+++ src/main/scala/net/anti344/jsonrecipes/plugins/RecipeHandler.scala
@@ -1,5 +1,5 @@
 /*
- * File Test.scala is part of JsonRecipes.
+ * File RecipeHandler.scala is part of JsonRecipes.
  * JsonRecipes is opensource Minecraft mod(released under LGPLv3), created by anti344.
  * Full licence information can be found in LICENCE and LICENCE.LESSER files in jar-file of the mod.
  * Copyright © 2014, anti344
--- src/main/scala/net/anti344/jsonrecipes/plugins/ic2/SimpleIC2MachineHandler.scala
+++ src/main/scala/net/anti344/jsonrecipes/plugins/ic2/SimpleIC2MachineHandler.scala
@@ -1,5 +1,5 @@
 /*
- * File CompressorHandler.scala is part of JsonRecipes.
+ * File SimpleIC2MachineHandler.scala is part of JsonRecipes.
  * JsonRecipes is opensource Minecraft mod(released under LGPLv3), created by anti344.
  * Full licence information can be found in LICENCE and LICENCE.LESSER files in jar-file of the mod.
  * Copyright © 2014, anti344
